d13f503c3df22eb5d73e38b270cff896a34bf660
nexryai <61890205+nexryai@users.noreply.github.com>
2025-06-26 13:38:53 +0000

    WIP

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,10 +6,16 @@ import QuakeMap from "@/components/QuakeMap.vue";
 const eventId = ref("");
 const isDebug = ref(false);
 const isLoading = ref(true);
+const currentEventIndex = ref(0);
+const isSeekableToNext = ref(false);
+const isSeekableToPrev = ref(false);
 
-const fetchEventId = async (index: number): Promise<string> => {
-    const resp = await fetch("https://quake-jade.vercel.app/api/events");
-    const events = (await resp.json() as { events: string[] }).events;
+let events: string[] | undefined = undefined;
+
+const fetchEventId = async (index: number): Promise<{id: string, isLatest: boolean, isFinal: boolean}> => {
+    if (!events) {
+        throw new Error("Events not loaded");
+    }
 
     // IDに_VXSE51または_VXSE53が含まれるもののうちindex番目のイベントを取得
     const filtered = events.filter((id) => id.includes("_VXSE51") || id.includes("_VXSE53"));
@@ -17,7 +23,15 @@ const fetchEventId = async (index: number): Promise<string> => {
         throw new Error("Index out of bounds");
     }
 
-    return filtered[index];
+    return { id: filtered[index], isLatest: index === 0, isFinal: index === filtered.length - 1 };
+};
+
+const updateEventId = async (index: number): Promise<void> => {
+    console.log(`Updating event ID to index: ${index}`);
+    const fetched = await fetchEventId(index);
+    eventId.value = fetched.id;
+    isSeekableToNext.value = !fetched.isLatest;
+    isSeekableToPrev.value = !fetched.isFinal;
 };
 
 onMounted(async () => {
@@ -29,8 +43,11 @@ onMounted(async () => {
         return;
     }
 
+    const resp = await fetch("https://quake-jade.vercel.app/api/events");
+    events = (await resp.json() as { events: string[] }).events;
+
     // 最初のイベントを取得
-    eventId.value = await fetchEventId(0);
+    await updateEventId(currentEventIndex.value);
 
     isLoading.value = false;
 });
@@ -39,5 +56,28 @@ onMounted(async () => {
 <template>
     <main>
         <QuakeMap v-if=!isLoading :is-debug="isDebug" :event-id="eventId" />
+        <div>
+            <div v-if="isLoading" class="loading">Loading...</div>
+            <div v-else class="controls">
+                <button
+                    :disabled="!isSeekableToNext"
+                    @click="async () => {
+                        currentEventIndex--;
+                        await updateEventId(currentEventIndex);
+                    }"
+                >
+                    Next
+                </button>
+                <button
+                    :disabled="!isSeekableToPrev"
+                    @click="async () => {
+                        currentEventIndex++;
+                        await updateEventId(currentEventIndex);
+                    }"
+                >
+                    Previous
+                </button>
+            </div>
+        </div>
     </main>
 </template>
